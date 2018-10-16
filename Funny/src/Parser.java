efcf622f8c4699999ebb3fb4557b3574212369a1
Alessandro Bianchi <alex24br@yahoo.it>
2018-10-16 10:05:27 +0200

    struttura parser per controllare token

--- Funny/src/Parser.java
+++ Funny/src/Parser.java
@@ -1,2 +1,22 @@
-public class Parser {
+import java.io.Reader;
+
+public class Parser
+{
+    private Tokenizer tokenizer;
+    private Token token;
+
+    public Parser(Reader reader)
+    {
+        tokenizer = new Tokenizer(reader);
+    }
+
+    public void next()
+    {
+        token = tokenizer.next();
+    }
+
+    public void execute()
+    {
+        //TODO: parser
+    }
 }
